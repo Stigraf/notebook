7635ea51a1b9f30cc02a4fd499401d9d4d0be8df
Stigraf <102538273+Stigraf@users.noreply.github.com>
2022-04-05 21:53:46 +0200

    Add files via upload

--- js.js
+++ js.js
@@ -1,26 +1,124 @@
+
+	let taskList = []
+	let counter = 0
+	updateUI()
+
 document.querySelector('#saveBut').addEventListener('click', () => {
-    
-        document.querySelector('#tasks').innerHTML += `
+   saveButPressed()
+})
+
+document.querySelector('#newtask input').addEventListener('keypress', e => {
+	if (e.key === 'Enter'){
+		saveButPressed()
+	}
+})
+
+function cleanInput() {
+document.querySelector('#newtask input').value = ""
+}
+
+function cleanUI() {
+ 	document.querySelector('#tasks').innerHTML = ""
+ }
+
+function generateTasks() {
+		console.log(taskList)
+        taskList.map(task => {
+        	
+        	const deleteId = getDeleteIdByItemId(task.id)
+        	const checked = task.isChecked ? 'checked' : ''
+        	const checkBoxId = getCheckBoxIdByItemId(task.id)
+
+        	document.querySelector('#tasks').innerHTML += `
             <div class="task">
 
-            	
+            	<input type="checkbox" id="${checkBoxId}" ${checked}>
                 <span id="taskname">
-                    ${document.querySelector('#newtask input').value}
+                   ${task.title} 
                 </span>
-                <button class="delete">delete
+                <button id=${deleteId}>delete
                     <i class="far fa-trash-alt"></i>
                 </button>
-            </div>
-        `;
-        
-        let current_tasks = document.querySelectorAll(".delete");
-        for(let i=0; i<current_tasks.length; i++){
-            current_tasks[i].onclick = function(){
-                this.parentNode.remove();
-            }
-        }
+        `
+       
+    })
 
-        
-         	
-    
-})
+        taskList.map(task => {
+			const deleteId = getDeleteIdByItemId(task.id)
+			const checkBoxId = getCheckBoxIdByItemId(task.id)
+			document.getElementById(checkBoxId).addEventListener('change', e => {
+				isCheckedChecked(task.id)
+
+
+			
+				 
+			}
+				)
+
+
+        	document.getElementById(deleteId).addEventListener('click', () => {
+        		removeTask(task.id)
+        		updateUI()
+        	})
+
+        	
+        })
+
+}
+function getCheckBoxIdByItemId(itemId){
+	return`checkBox_${itemId}`
+}
+
+function getDeleteIdByItemId(itemId) {
+	return  `delete_${itemId}`
+}
+
+function isCheckedChecked(id) {
+	console.log(`isChecked, id:${id}`)
+
+	
+	let result = taskList.find(myId => myId.id ==id)
+	let itemId = taskList.indexOf(result)
+	console.log(itemId)
+	let isChecked = taskList[itemId].isChecked 
+	taskList[itemId].isChecked = !isChecked
+	
+
+
+	
+
+}
+
+
+function removeTask(id) {
+	console.log(`removeTask, id: ${id}`)
+	taskList = taskList.filter(function(value, index, arr){
+		return value.id != id
+	})
+}
+
+function updateUI() {
+			cleanUI()
+        	generateTasks()
+}
+
+function saveButPressed() {
+	    let text = document.querySelector('#newtask input').value
+    if (text == "") {
+    	alert('input is empty')
+    }
+    else {
+    let task = { title: text, isChecked: false, id: counter }
+    taskList.push(task)
+    counter += 1
+
+updateUI()
+cleanInput()  	
+ }
+
+}
+
+
+function setFocusToTextBox() {
+document.querySelector('#newtask input').focus();
+}
