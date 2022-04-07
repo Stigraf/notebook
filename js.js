e4620f290bdaee2c93e7c02b4b8446c87c4a2c32
Stigraf <102538273+Stigraf@users.noreply.github.com>
2022-04-07 14:55:56 +0200

    Add files via upload

--- js.js
+++ js.js
@@ -28,19 +28,23 @@ function generateTasks() {
         	const deleteId = getDeleteIdByItemId(task.id)
         	const checked = task.isChecked ? 'checked' : ''
         	const checkBoxId = getCheckBoxIdByItemId(task.id)
+        	const taskNameId = getTaskNameByItemId(task.id)
 
         	document.querySelector('#tasks').innerHTML += `
             <div class="task">
 
             	<input type="checkbox" id="${checkBoxId}" ${checked}>
-                <span id="taskname">
+                <span id="${taskNameId}">
                    ${task.title} 
                 </span>
                 <button id=${deleteId}>delete
                     <i class="far fa-trash-alt"></i>
                 </button>
-        `
-       
+			`;
+			isThrough(task.id)
+			
+
+       			
     })
 
         taskList.map(task => {
@@ -48,12 +52,14 @@ function generateTasks() {
 			const checkBoxId = getCheckBoxIdByItemId(task.id)
 			document.getElementById(checkBoxId).addEventListener('change', e => {
 				isCheckedChecked(task.id)
+				isThrough(task.id)
+		
 
-
-			
 				 
 			}
 				)
+			
+
 
 
         	document.getElementById(deleteId).addEventListener('click', () => {
@@ -65,8 +71,12 @@ function generateTasks() {
         })
 
 }
+
 function getCheckBoxIdByItemId(itemId){
 	return`checkBox_${itemId}`
+}
+function getTaskNameByItemId(itemId){
+	return`taskname_${itemId}`
 }
 
 function getDeleteIdByItemId(itemId) {
@@ -75,18 +85,15 @@ function getDeleteIdByItemId(itemId) {
 
 function isCheckedChecked(id) {
 	console.log(`isChecked, id:${id}`)
-
 	
 	let result = taskList.find(myId => myId.id ==id)
 	let itemId = taskList.indexOf(result)
 	console.log(itemId)
 	let isChecked = taskList[itemId].isChecked 
 	taskList[itemId].isChecked = !isChecked
-	
 
 
 	
-
 }
 
 
@@ -100,6 +107,7 @@ function removeTask(id) {
 function updateUI() {
 			cleanUI()
         	generateTasks()
+
 }
 
 function saveButPressed() {
@@ -122,3 +130,26 @@ cleanInput()
 function setFocusToTextBox() {
 document.querySelector('#newtask input').focus();
 }
+
+ function isThrough(id) {
+ 	let result = taskList.find(myId => myId.id == id)
+	let itemId = taskList.indexOf(result)
+	let isChecked = taskList[itemId].isChecked 
+	textId = taskList[itemId].id 
+	if (taskList[itemId].isChecked == true){
+		document.getElementById(`taskname_${textId}`).classList.add('through')
+	} else {
+		document.getElementById(`taskname_${textId}`).classList.remove('through')
+	}
+
+
+
+
+
+
+ // 	if (taskList[task.id].isChecked == true) {
+	// document.getElementById(`taskname_${itemId}`).classList.add('through')
+	// } else { 
+	// document.getElementById(`taskname_${itemId}`).classList.remove('through')
+	// }
+ }
