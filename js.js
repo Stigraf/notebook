572c515358b78b509c14edf803d2317e4aa931db
Stigraf <102538273+Stigraf@users.noreply.github.com>
2022-04-15 17:33:02 +0200

    Add files via upload

--- js.js
+++ js.js
@@ -1,21 +1,16 @@
 let taskList = []
 let counter = 0
-const saveButton = document.getElementById('saveBut')
-const inputTask = document.querySelector('#newtask input')
+const saveButton = document.getElementById('saveButton')
+const inputTask = document.querySelector('#new-task input')
 
 
 updateUI()
 saveButton.disabled = true
 inputTask.addEventListener('input', (event) => {
-    saveButton.disabled = event.target.value === '';
-    document.getElementById('errorAlert').innerHTML = `<br>`
+    saveButton.disabled = event.target.value.trim() === '';
+    document.getElementById('errorAlert').innerHTML = ``
     if (taskList.some(e => e.title === inputTask.value.trim())) {
-        document.getElementById('errorAlert').innerHTML = inputTask.value + ` task already exist`
-        let itemId = taskList.findIndex(e => e.title === inputTask.value.trim())
-        let textId = taskList[itemId].id
-        document.getElementById(`taskname_${textId}`).classList.add('red')
-        setTimeout(() => document.getElementById(`taskname_${textId}`).classList.remove('red'), 1000)
-        saveButton.disabled = true
+        errorAlert()
     }
 
 })
@@ -26,13 +21,9 @@ saveButton.addEventListener('click', () => {
 })
 
 inputTask.addEventListener('keypress', e => {
-    if (e.key === 'Enter' && saveButton.disabled === false) {
-
-        saveButPressed()
-
-    }
-
-
+        if (e.key === 'Enter' && saveButton.disabled === false && inputTask === document.activeElement) {
+            saveButPressed()
+        }
 })
 
 
@@ -52,24 +43,23 @@ function generateTasks() {
             <div class="task">
 
             	<input type="checkbox" id="${checkBoxId}" ${checked}>
-                <span id="${taskNameId}">
-                   ${task.title} 
-                </span>
-                <button class="delBut" id=${deleteId}>delete
+                <input class="task-Class" id="${taskNameId}" value="${task.title}">
+                <button class="delete-Button" id=${deleteId}>delete
                     <i class="far fa-trash-alt"></i>
                 </button>
                 <hr>
 			`;
         isThrough(task.id)
+
         saveButton.disabled = true
         setFocusToTextBox()
 
 
     })
-
     taskList.map(task => {
         const deleteId = getDeleteIdByItemId(task.id)
         const checkBoxId = getCheckBoxIdByItemId(task.id)
+        taskChange(task.id)
         document.getElementById(checkBoxId).addEventListener('change', () => {
                 isCheckedChecked(task.id)
                 isThrough(task.id)
@@ -78,17 +68,59 @@ function generateTasks() {
 
             }
         )
-
-
         document.getElementById(deleteId).addEventListener('click', () => {
             removeTask(task.id)
             updateUI()
         })
 
 
+
+
+
     })
 
 }
+
+function taskChange(id) {
+
+
+    let result = taskList.find(myId => myId.id === id)
+    let itemId = taskList.indexOf(result)
+    const taskName = document.getElementById(`taskname_${itemId}`)
+    const errorAlert = document.getElementById('errorAlert')
+
+    taskName.addEventListener('input', () => {
+        if (taskName.value.trim() === '') {
+            errorAlert.innerHTML = taskName.value + ` task is empty`
+        } else {
+            if (taskList.some(e => e.title === taskName.value.trim()) && taskName.value.trim() !== taskList[itemId].title) {
+                errorAlert.innerHTML = taskName.value + ` task already exist`
+
+                let itemId2 = taskList.findIndex(e => e.title === taskName.value.trim());
+                let textId2 = taskList[itemId2].id;
+                if (taskName.value.trim() !== taskList[itemId].title) {
+                    document.getElementById(`taskname_${textId2}`).classList.add('red')
+                    setTimeout(() => document.getElementById(`taskname_${textId2}`).classList.remove('red'), 1000)
+                }
+            } else {
+                errorAlert.innerHTML = ""
+            }
+        }
+    })
+    if (taskName) {
+        taskName.addEventListener("change", () => {
+            if (taskList.some(e => e.title === taskName.value.trim()) || taskName.value.trim() === '') {
+                    updateUI()
+            } else {
+                taskList[itemId].title = taskName.value.trim()
+                updateUI()
+            }
+            errorAlert.innerHTML = ''
+        })
+
+    }
+}
+
 
 function getCheckBoxIdByItemId(itemId) {
     return `checkBox_${itemId}`
@@ -108,7 +140,6 @@ function cleanInput() {
 
 function isCheckedChecked(id) {
     console.log(`isChecked, id:${id}`)
-
     let result = taskList.find(myId => myId.id === id)
     let itemId = taskList.indexOf(result)
     console.log(itemId)
@@ -158,7 +189,6 @@ function setFocusToTextBox() {
 function isThrough(id) {
     let result = taskList.find(myId => myId.id === id)
     let itemId = taskList.indexOf(result)
-
     let textId = taskList[itemId].id
     if (taskList[itemId].isChecked === true) {
         document.getElementById(`taskname_${textId}`).classList.add('through')
@@ -166,10 +196,18 @@ function isThrough(id) {
         document.getElementById(`taskname_${textId}`).classList.remove('through')
     }
 
-
     // 	if (taskList[task.id].isChecked == true) {
     // document.getElementById(`taskname_${itemId}`).classList.add('through')
     // } else {
     // document.getElementById(`taskname_${itemId}`).classList.remove('through')
     // }
+}
+
+function errorAlert() {
+    document.getElementById('errorAlert').innerHTML = inputTask.value + ` task already exist`
+    let itemId = taskList.findIndex(e => e.title === inputTask.value.trim());
+    let textId = taskList[itemId].id;
+    document.getElementById(`taskname_${textId}`).classList.add('red')
+    setTimeout(() => document.getElementById(`taskname_${textId}`).classList.remove('red'), 1000)
+    saveButton.disabled = true
 }
